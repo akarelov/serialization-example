8c4be6da49ee17d5c6ad1db78019f59ea72a6feb
akarelov <karelov.artyom@gmail.com>
2020-01-23 15:06:45 +0300

    adding method for serial/deserial file

--- src/main/java/com/akarelov/serialization/Application.java
+++ src/main/java/com/akarelov/serialization/Application.java
@@ -1,7 +1,25 @@
 package com.akarelov.serialization;
 
-public class Application {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Application {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        String filename = "book_number_1.cer";
+        serializationFile(filename);
     }
+
+    public static void serializationFile(String filename) throws IOException, ClassNotFoundException {
+        Book book = new Book("book number 1", "description of book number 1");
+
+        OutputStream outputStream = new FileOutputStream(filename);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+        objectOutputStream.writeObject(book);
+
+        InputStream inputStream = new FileInputStream(filename);
+        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+        Book bookFromFile = (Book) objectInputStream.readObject();
+
+        System.out.println(book.getTitle() + " " + bookFromFile.getDescription());
+    }
+
 }
